1f655b14725e9f788e99bffcc66a69745ae36609
Danilo Vitoriano <vitoriano08@gmail.com>
2018-09-05 14:04:41 -0300

    add pagination and zapitemstories

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,8 +2,17 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 
-import { Button } from "@storybook/react/demo";
+import ZapItem from "../components/ZapItem";
+import Pagination from "../components/Pagination";
 
-storiesOf("Button", module).add("with text", () => (
-  <Button>Hello Button</Button>
-));
+var data = {
+  id: 7878999988
+};
+
+var exampleItems = [...Array(150).keys()].map(i => ({ id: i + 1 }));
+
+storiesOf("Pagination", module).add(
+  "with 20 itens per page and 10 pages max",
+  () => <Pagination items={exampleItems} onChangePage={() => null} />
+);
+storiesOf("ZapItem", module).add("with infos", () => <ZapItem data={data} />);
