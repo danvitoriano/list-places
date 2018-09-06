a99f20db5ed5104bd938dfde6d422300130f9308
Danilo Vitoriano <vitoriano08@gmail.com>
2018-09-06 11:14:13 -0300

    add image component

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,10 +5,18 @@ import { storiesOf } from "@storybook/react";
 import Card from "../components/Card";
 import Pagination from "../components/Pagination";
 import Text from "../components/Text";
+import Image from "../components/Image";
 
 const data = {
   textLabel: "My Sample Text",
   id: 787654456,
+  images: [
+    "https://resizedimgs.vivareal.com/crop/400x300/vr.images.sp/285805119ab0761500127aebd8ab0e1d.jpg",
+    "https://resizedimgs.vivareal.com/crop/400x300/vr.images.sp/4af1656b66b9e12efff6ce06f51926f6.jpg",
+    "https://resizedimgs.vivareal.com/crop/400x300/vr.images.sp/895f0d4ce1e641fd5c3aad48eff83ac8.jpg",
+    "https://resizedimgs.vivareal.com/crop/400x300/vr.images.sp/e7b5cce2d9aee78867328dfa0a7ba4c6.jpg",
+    "https://resizedimgs.vivareal.com/crop/400x300/vr.images.sp/d833da4cdf6b25b7acf3ae0710d3286d.jpg"
+  ],
   address: {
     geoLocation: {
       location: {
@@ -35,4 +43,7 @@ storiesOf("Text", module).add("h1", () => (
 ));
 storiesOf("Text", module).add("price", () => (
   <Text type="price" label={data.pricingInfos.price} />
+));
+storiesOf("Image", module).add("default image", () => (
+  <Image src={data.images[0]} label="Image" />
 ));
