065f537963e708e264d53f5bf53da3ee52e30d80
Danilo Vitoriano <vitoriano08@gmail.com>
2018-09-06 13:50:51 -0300

    add slider

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,36 +1,7 @@
 import React from "react";
-import { css } from "glamor";
-
-const styles = {
-  container: css({
-    display: "grid",
-    gridTemplateColumns: "auto",
-    width: 400,
-    height: 300,
-    margin: "0 auto",
-    "> img": {
-      verticalAlign: "middle",
-      width: 400,
-      height: 300
-    }
-  })
-};
 
 function Image(props) {
-  switch (props.type) {
-    case "mini":
-      return (
-        <div {...styles.container}>
-          <img src={props.src} alt={props.label ? props.label : null} />
-        </div>
-      );
-    default:
-      return (
-        <div {...styles.container}>
-          <img src={props.src} alt={props.label ? props.label : null} />
-        </div>
-      );
-  }
+  return <img src={props.src} alt={props.label ? props.label : null} />;
 }
 
 export default Image;
